ed5231d35c5727f08de5106fd07d9fd3cfd9ece1
mari3-3 <0somethingsomething0@gmail.com>
2024-10-16 08:07:49 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+#Tyler, Martide, Mariet, Oluseyi Group Code
+for count in range(1,101):
+  if count % 3 == 0 and count % 5 == 0:
+    print("Fizz-Buzz")
+  elif count % 3 == 0:
+    print("Fizz")
+  elif count % 5 == 0:
+    print("Buzz")
+  else:
+    print(count)
